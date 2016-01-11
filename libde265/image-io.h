fd5f80cba37b2bad6402ae272172ee48f07f8310
Debian Multimedia Maintainers <pkg-multimedia-maintainers@lists.alioth.debian.org>
2016-01-11 18:12:19 +0000

    only_export_decoder_api
    Gbp-Pq: Name only_export_decoder_api.patch

--- libde265/image-io.h
+++ libde265/image-io.h
@@ -30,17 +30,17 @@
 class ImageSource
 {
  public:
-  LIBDE265_API ImageSource() { }
-  virtual LIBDE265_API ~ImageSource() { }
+  ImageSource() { }
+  virtual ~ImageSource() { }
 
   //enum ImageStatus { Available, Waiting, EndOfVideo };
 
   //virtual ImageStatus  get_status() = 0;
-  virtual LIBDE265_API de265_image* get_image(bool block=true) = 0;
-  virtual LIBDE265_API void skip_frames(int n) = 0;
+  virtual de265_image* get_image(bool block=true) = 0;
+  virtual void skip_frames(int n) = 0;
 
-  virtual LIBDE265_API int get_width() const = 0;
-  virtual LIBDE265_API int get_height() const = 0;
+  virtual int get_width() const = 0;
+  virtual int get_height() const = 0;
 };
 
 
@@ -48,17 +48,17 @@ class ImageSource
 class ImageSource_YUV : public ImageSource
 {
  public:
- LIBDE265_API ImageSource_YUV() : mFH(NULL) { }
-  virtual LIBDE265_API ~ImageSource_YUV();
+  ImageSource_YUV() : mFH(NULL) { }
+  virtual ~ImageSource_YUV();
 
-  bool LIBDE265_API set_input_file(const char* filename, int w,int h);
+  bool set_input_file(const char* filename, int w,int h);
 
   //virtual ImageStatus  get_status();
-  virtual LIBDE265_API de265_image* get_image(bool block=true);
-  virtual LIBDE265_API void skip_frames(int n);
+  virtual de265_image* get_image(bool block=true);
+  virtual void skip_frames(int n);
 
-  virtual LIBDE265_API int get_width() const { return width; }
-  virtual LIBDE265_API int get_height() const { return height; }
+  virtual int get_width() const { return width; }
+  virtual int get_height() const { return height; }
 
  private:
   FILE* mFH;
@@ -75,20 +75,20 @@ class ImageSource_YUV : public ImageSource
 class ImageSink
 {
  public:
-  virtual LIBDE265_API ~ImageSink() { }
+  virtual ~ImageSink() { }
 
-  virtual LIBDE265_API void send_image(const de265_image* img) = 0;
+  virtual void send_image(const de265_image* img) = 0;
 };
 
 class ImageSink_YUV : public ImageSink
 {
  public:
- LIBDE265_API ImageSink_YUV() : mFH(NULL) { }
-  LIBDE265_API ~ImageSink_YUV();
+  ImageSink_YUV() : mFH(NULL) { }
+  ~ImageSink_YUV();
 
-  bool LIBDE265_API set_filename(const char* filename);
+  bool set_filename(const char* filename);
 
-  virtual LIBDE265_API void send_image(const de265_image* img);
+  virtual void send_image(const de265_image* img);
 
  private:
   FILE* mFH;
@@ -99,21 +99,21 @@ class ImageSink_YUV : public ImageSink
 class PacketSink
 {
  public:
-  virtual LIBDE265_API ~PacketSink() { }
+  virtual ~PacketSink() { }
 
-  virtual LIBDE265_API void send_packet(const uint8_t* data, int n) = 0;
+  virtual void send_packet(const uint8_t* data, int n) = 0;
 };
 
 
 class PacketSink_File : public PacketSink
 {
  public:
- LIBDE265_API PacketSink_File() : mFH(NULL) { }
-  virtual LIBDE265_API ~PacketSink_File();
+  PacketSink_File() : mFH(NULL) { }
+  virtual ~PacketSink_File();
 
-  LIBDE265_API void set_filename(const char* filename);
+  void set_filename(const char* filename);
 
-  virtual LIBDE265_API void send_packet(const uint8_t* data, int n);
+  virtual void send_packet(const uint8_t* data, int n);
 
  private:
   FILE* mFH;
