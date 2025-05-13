95ac4ade8d63809a116f67bca4ad7826428d68ee
Joachim Bauch <bauch@struktur.de>
2025-05-13 09:32:27 +0200

    Only export symbols defined in the decoder API.
    The encoder API is not final yet, so upstream exports all symbols to make
    development easier. For packaging we only want to expose the public API.
    ===================================================================
    
    Gbp-Pq: Name only_export_decoder_api.patch

--- libde265/image-io.cc
+++ libde265/image-io.cc
@@ -208,7 +208,7 @@ PacketSink_File::PacketSink_File()
 }
 
 
-LIBDE265_API PacketSink_File::~PacketSink_File()
+PacketSink_File::~PacketSink_File()
 {
   if (mFH) {
     fclose(mFH);
@@ -216,7 +216,7 @@ LIBDE265_API PacketSink_File::~PacketSink_File()
 }
 
 
-LIBDE265_API void PacketSink_File::set_filename(const char* filename)
+void PacketSink_File::set_filename(const char* filename)
 {
   assert(mFH==NULL);
 
@@ -224,7 +224,7 @@ LIBDE265_API void PacketSink_File::set_filename(const char* filename)
 }
 
 
-LIBDE265_API void PacketSink_File::send_packet(const uint8_t* data, int n)
+void PacketSink_File::send_packet(const uint8_t* data, int n)
 {
   uint8_t startCode[3];
   startCode[0] = 0;
