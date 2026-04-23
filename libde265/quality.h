16dd1cdf0d7cb9fd4a48eb2a451accc79656e7c4
Joachim Bauch <bauch@struktur.de>
2026-04-23 12:45:20 +0200

    Only export symbols defined in the decoder API.
    The encoder API is not final yet, so upstream exports all symbols to make
    development easier. For packaging we only want to expose the public API.
    
    Gbp-Pq: Name only_export_decoder_api.patch

--- libde265/quality.h
+++ libde265/quality.h
@@ -26,11 +26,11 @@
 #include <libde265/image.h>
 
 
-LIBDE265_API uint32_t SSD(const uint8_t* img, int imgStride,
+uint32_t SSD(const uint8_t* img, int imgStride,
                           const uint8_t* ref, int refStride,
                           int width, int height);
 
-LIBDE265_API uint32_t SAD(const uint8_t* img, int imgStride,
+uint32_t SAD(const uint8_t* img, int imgStride,
                           const uint8_t* ref, int refStride,
                           int width, int height);
 
@@ -41,7 +41,7 @@ LIBDE265_API double MSE(const uint8_t* img, int imgStride,
 LIBDE265_API double PSNR(double mse);
 
 
-LIBDE265_API uint32_t compute_distortion_ssd(const de265_image* img1, const de265_image* img2,
+uint32_t compute_distortion_ssd(const de265_image* img1, const de265_image* img2,
                                              int x0, int y0, int log2size, int cIdx);
 
 #endif
